072cf49dae388fcc8e4e50cbf027cf3db7456447
Abhinav26398 <109171494+Abhinav26398@users.noreply.github.com>
2024-02-04 13:08:53 +0530

    Add files via upload

---
+++ mvcpractice/src/test/java/com/abhinav/mvcpractice/MvcpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhinav.mvcpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
